0f9bbd39f97d2aa5b498aadaef3069cd74022c99
AndreiZuenkov <andreyzuenkov@gmail.com>
2021-11-12 22:29:36 +0300

    add methods

---
+++ src/com/neostudy/Main.java
@@ -0,0 +1,67 @@
+package com.neostudy;
+
+public class Main {
+    public static void main(String[] args) {
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt = 2147483647;
+        long myLong = 9223372036854775807L;
+        float myFloat = 7.75f;
+        double myDouble = 9.29;
+        char myChar = 'N';
+        boolean myBoolean = true;
+        String myString = "Hello NeoStudy";
+
+
+        System.out.println(calculate(1, 2, 6, 3));
+        System.out.println(checkSum(5, 6));
+        checkInt(1);
+        System.out.println(checkIsNegative(-1));
+        printName("Andrey");
+        checkYear(2300);
+
+    }
+
+    public static int calculate(int a, int b, int c, int d) {
+        int result;
+
+        result = a * (b + (c / d));
+
+        return result;
+    }
+
+    public static boolean checkSum(double first, double second) {
+
+        if (first + second >= 10 && first + second <= 20) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void checkInt(int number) {
+
+        if (number >= 0) {
+            System.out.println("В метод передали положительное число");
+        } else
+            System.out.println("В метод передали отрицательно число");
+
+    }
+
+    public static boolean checkIsNegative(int number) {
+        if (number < 0) {
+            return true;
+        } else return false;
+    }
+
+    public static void printName(String name) {
+        System.out.println("Привет, " + name);
+    }
+
+    public static void checkYear(int year) {
+
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else
+            System.out.println(year + " год не является високосным");
+    }
+}
